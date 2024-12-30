350a6fb84cccaf8e6fc973ef7ae84be673349acf
Alex Mickelson <alexmickelson96@gmail.com>
2024-12-30 12:21:36 -0700

    home-server nix

--- nix/home-server.nix
+++ nix/home-server.nix
@@ -337,5 +337,4 @@ k9s:
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
