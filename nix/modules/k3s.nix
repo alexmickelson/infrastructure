1458dfe23b34bc14f08ee730ea42b660e4f5a547
Alex Mickelson <alexmickelson96@gmail.com>
2025-01-13 21:49:27 -0700

    proper ip

--- nix/modules/k3s.nix
+++ nix/modules/k3s.nix
@@ -7,6 +7,6 @@
   services.k3s.extraFlags = toString [
     # "--debug" # Optionally add additional args to k3s
     "--disable=traefik"
-    "--tls-san 100.96.241.36"
+    "--tls-san 100.122.128.107"
   ];
 }
