089f9b3f5524b81dc2847d5480c8e4d1d08bd933
Alex Mickelson <alexmickelson96@gmail.com>
2025-01-13 21:48:22 -0700

    traefik

--- nix/modules/k3s.nix
+++ nix/modules/k3s.nix
@@ -6,6 +6,7 @@
   services.k3s.role = "server";
   services.k3s.extraFlags = toString [
     # "--debug" # Optionally add additional args to k3s
+    "--disable=traefik"
     "--tls-san 100.96.241.36"
   ];
 }
