c49100a3589717c19157552c4e8313e74745a724
Alex Mickelson <alexmickelson96@gmail.com>
2025-02-07 21:15:35 -0700

    update

--- nix/alex-desktop.nix
+++ nix/alex-desktop.nix
@@ -89,6 +89,7 @@
     tmux
     libguestfs-with-appliance
     iperf
+    mangohud
   ];
   services.tailscale.enable = true;
   services.openssh.enable = true;
@@ -103,10 +104,15 @@
     enable = true;
     capSysNice = true;
   };
+  programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  networking.firewall.enable = false;
 
   #services.sunshine = {
   #  enable = true;
@@ -141,7 +147,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    open = true;
+    open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
     powerManagement.enable = false;
