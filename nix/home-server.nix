76c53aee4657092282ab5bb397867a65751a798f
Alex Mickelson <alexmickelson96@gmail.com>
2025-02-13 22:05:32 -0700

    update

--- nix/home-server.nix
+++ nix/home-server.nix
@@ -151,7 +151,7 @@
     };
   };
   nvram = [ 
-    "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", 
+    "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd"
     "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" 
   ]
 
