a7bd9514e62b2993b993d92b7272ac63cae5388a
Alex Mickelson <alexmickelson96@gmail.com>
2025-02-13 21:42:45 -0700

    better ovmf

--- nix/home-server.nix
+++ nix/home-server.nix
@@ -126,7 +126,22 @@
   };
 
   # virtualization stuff
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
   powerManagement.powertop.enable = true;
   powerManagement.enable = true;
   powerManagement.cpuFreqGovernor = "powersave";
