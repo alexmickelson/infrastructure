ba70bd2cd4887e869b2d463a7de2999fefd97d89
Alex Mickelson <alexmickelson96@gmail.com>
2025-03-29 08:55:06 -0600

    k3s

--- nix/modules/k3s.nix
+++ nix/modules/k3s.nix
@@ -2,14 +2,17 @@
 { ... }:
 
 {
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    # "--debug" # Optionally add additional args to k3s
-    "--disable=traefik"
-    "--bind-address 100.122.128.107"
-    "--tls-san 100.122.128.107"
-  ];
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      # "--debug" # Optionally add additional args to k3s
+      "--disable=traefik"
+      "--bind-address 100.122.128.107"
+      "--tls-san 100.122.128.107"
+    ];
+    serverAddr = "https://100.122.128.107:6443"
+  };
   networking.firewall.allowedTCPPorts = [
     443
     80
