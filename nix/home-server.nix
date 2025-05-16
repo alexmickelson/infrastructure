61c78007be234192b3a2898a2b1a40c5232a0ef9
Alex Mickelson <alexmickelson96@gmail.com>
2025-05-15 21:01:05 -0600

    no overrides

--- nix/home-server.nix
+++ nix/home-server.nix
@@ -149,10 +149,10 @@
       swtpm.enable = true;
       ovmf = {
         enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
+        # packages = [(pkgs.OVMF.override {
+        #   secureBoot = true;
+        #   tpmSupport = true;
+        # }).fd];
       };
     };
   };
