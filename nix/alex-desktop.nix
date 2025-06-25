0341807001ccfe85e4f8f9835c5f08de36b70aa8
Alex Mickelson <alexmickelson96@gmail.com>
2025-06-24 19:04:55 -0600

    steam through flatpak

--- nix/alex-desktop.nix
+++ nix/alex-desktop.nix
@@ -116,13 +116,13 @@
     capSysNice = true;
   };
   programs.gamemode.enable = true;
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+  # programs.steam = {
+  #   enable = true;
+  #   gamescopeSession.enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  # };
   networking.firewall.enable = false;
 
   #services.sunshine = {
