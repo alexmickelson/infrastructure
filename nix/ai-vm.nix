76b76f4c0c43dd7e2261d0c401a5dd5b9e53ea42
Alex Mickelson <alexmickelson96@gmail.com>
2025-10-29 19:58:04 -0600

    different sunshine

--- nix/ai-vm.nix
+++ nix/ai-vm.nix
@@ -115,11 +115,11 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
-    serviceConfig = {
-      After = [ "graphical-session.target" ];
-      Wants = [ "graphical-session.target" ];
-      BindsTo = [ "graphical-session.target" ];
-    };
+    # serviceConfig = {
+    #   After = [ "graphical-session.target" ];
+    #   Wants = [ "graphical-session.target" ];
+    #   BindsTo = [ "graphical-session.target" ];
+    # };
   };
 
   xdg.portal.enable = true;
