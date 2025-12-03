c62f9f1aa3811a6fd8e3a88de7389f42c821d379
Alex Mickelson <alexmickelson96@gmail.com>
2025-12-03 16:02:28 -0700

    xrdp

--- nix/ai-server-1.nix
+++ nix/ai-server-1.nix
@@ -47,13 +47,17 @@
   xdg.portal.config.common.default = [ "gnome" ];
 
 
-  services.xrdp.enable = true;
+  # services.xrdp.enable = true;
 
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  # services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  # services.gnome.gnome-remote-desktop.enable = true;
+  # services.xrdp.openFirewall = true;
+  # services.displayManager.autoLogin.enable = false;
+  # services.getty.autologinUser = null;
   services.gnome.gnome-remote-desktop.enable = true;
-  services.xrdp.openFirewall = true;
-  services.displayManager.autoLogin.enable = false;
-  services.getty.autologinUser = null;
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = [ "graphical.target" ];
+  };
 
 
   # Configure keymap in X11
