04b319d43809668ba1d6b82c9d4bb73d819611e4
Alex Mickelson <alexmickelson96@gmail.com>
2025-12-03 16:00:08 -0700

    xrdp

--- nix/ai-server-1.nix
+++ nix/ai-server-1.nix
@@ -36,10 +36,10 @@
   services.xserver.enable = true;
   services.displayManager = {
     gdm.enable = true;
-    autoLogin = {
-      enable = true;
-      user = "alex";
-    };
+    # autoLogin = {
+    #   enable = true;
+    #   user = "alex";
+    # };
   };
   services.xserver.desktopManager.gnome.enable = true;
   xdg.portal.enable = true;
@@ -47,10 +47,14 @@
   xdg.portal.config.common.default = [ "gnome" ];
 
 
-  services.gnome.gnome-remote-desktop.enable = true;
   services.xrdp.enable = true;
+
   services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  services.gnome.gnome-remote-desktop.enable = true;
   services.xrdp.openFirewall = true;
+  services.displayManager.autoLogin.enable = false;
+  services.getty.autologinUser = null;
+
 
   # Configure keymap in X11
   services.xserver.xkb = {
