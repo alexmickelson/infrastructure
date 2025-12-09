8ca99235d99e2797bfd8741af67e6f3666e1d267
Alex Mickelson <alexmickelson96@gmail.com>
2025-12-09 09:54:49 -0700

    experimental features

--- nix/ai-server-1.nix
+++ nix/ai-server-1.nix
@@ -6,7 +6,8 @@
       <home-manager/nixos>
       # /etc/nixos/cachix.nix
     ];
-
+  nix.extraExperimentalFeatures = [ "flakes" "nix-command" ];
+  
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
