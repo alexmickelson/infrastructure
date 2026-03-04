e08252dc17f8d5b3ce32f6cac97c249b96218e7b
Alex Mickelson <alexmickelson96@gmail.com>
2026-03-03 21:46:41 -0700

    no wayland again

--- nix/tv-computer.nix
+++ nix/tv-computer.nix
@@ -39,9 +39,12 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # services.xserver.enable = true;
+  # services.displayManager.gdm.enable = true;
+  # services.desktopManager.gnome.enable = true;
   services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.xkb = {
     layout = "us";
     variant = "";
