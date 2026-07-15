9f84181faebdbb4dcf3e766778089bed117fed49
Alex Mickelson <alexmickelson96@gmail.com>
2026-07-15 12:18:37 -0600

    go migration tool

--- kubernetes/forgejo/migrate-from-gitea/main.go
+++ kubernetes/forgejo/migrate-from-gitea/main.go
@@ -1,8 +1,18 @@
 package main
+import (
+	"fmt"
+	"log"
+	"os"
 
-import "fmt"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	thing := "this"
-	fmt.Println(thing)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+	gitea_token := os.Getenv("GITEA_TOKEN") 
+	forgejo_token := os.Getenv("FORGEJO_TOKEN")
+	fmt.Printf("gitea token %s, forgejo token %s", gitea_token, forgejo_token)
 }
