7c5437bcfdb1339f662d7610a3472770977889c9
Alex Mickelson <alexmickelson96@gmail.com>
2026-07-20 11:12:59 -0600

    renovate json

--- renovate.sh
+++ renovate.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -a
 source .env
 set +a
@@ -14,11 +13,17 @@ docker pull renovate/renovate:latest
 #  -e RENOVATE_AUTO_MERGE=true \
 #  -e RENOVATE_IGNORE_TESTS=true \
 #  renovate/renovate:latest
+# docker run -it --rm \
+#   -e RENOVATE_TOKEN="$RENOVATE_GITHUB_TOKEN" \
+#   -e RENOVATE_PLATFORM=github \
+#   -e RENOVATE_REPOSITORIES=alexmickelson/office-infrastructure \
+#   -e RENOVATE_AUTO_MERGE=true \
+#   -e RENOVATE_IGNORE_TESTS=true \
+#   renovate/renovate:latest
 docker run -it --rm \
-  -e RENOVATE_TOKEN="$RENOVATE_GITHUB_TOKEN" \
-  -e RENOVATE_PLATFORM=github \
-  -e RENOVATE_REPOSITORIES=alexmickelson/office-infrastructure \
-  -e RENOVATE_AUTO_MERGE=true \
-  -e RENOVATE_IGNORE_TESTS=true \
+  -e RENOVATE_TOKEN="$RENOVATE_FORGEJO_TOKEN" \
+  -e RENOVATE_PLATFORM=forgejo \
+  -e RENOVATE_ENDPOINT=https://forgejo.alexmickelson.guru \
+  -e RENOVATE_REPOSITORIES=alex/infrastructure \
   renovate/renovate:latest
   # -e LOG_LEVEL=debug \
