948a2a37904394b91c6b7bb157d64a21c7f138b9
Alex Mickelson <alexmickelson96@gmail.com>
2026-07-20 13:46:51 -0600

    debug flag

--- renovate.sh
+++ renovate.sh
@@ -3,8 +3,16 @@ set -a
 source .env
 set +a
 
-docker pull renovate/renovate:latest
+DEBUG_ENABLED=false
+for arg in "$@"; do
+  case $arg in
+    -d|--debug)
+      DEBUG_ENABLED=true
+      ;;
+  esac
+done
 
+docker pull renovate/renovate:latest
 
 #docker run -it --rm \
 #  -e RENOVATE_TOKEN="$RENOVATE_GITHUB_TOKEN" \
@@ -20,7 +28,14 @@ docker pull renovate/renovate:latest
 #   -e RENOVATE_AUTO_MERGE=true \
 #   -e RENOVATE_IGNORE_TESTS=true \
 #   renovate/renovate:latest
+
+DOCKER_DEBUG_FLAG=""
+if [ "$DEBUG_ENABLED" = true ]; then
+  DOCKER_DEBUG_FLAG="-e LOG_LEVEL=debug "
+fi
+
 docker run --rm \
+  $DOCKER_DEBUG_FLAG \
   -e RENOVATE_TOKEN="$RENOVATE_FORGEJO_TOKEN" \
   -e RENOVATE_PLATFORM=forgejo \
   -e RENOVATE_ENDPOINT=https://forgejo.alexmickelson.guru \
