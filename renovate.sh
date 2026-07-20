cec53304c08115d90d7f54d1466a1d2eec15e765
Alex Mickelson <alexmickelson96@gmail.com>
2026-07-20 11:19:49 -0600

    update renovate config

--- renovate.sh
+++ renovate.sh
@@ -20,7 +20,7 @@ docker pull renovate/renovate:latest
 #   -e RENOVATE_AUTO_MERGE=true \
 #   -e RENOVATE_IGNORE_TESTS=true \
 #   renovate/renovate:latest
-docker run -it --rm \
+docker run --rm \
   -e RENOVATE_TOKEN="$RENOVATE_FORGEJO_TOKEN" \
   -e RENOVATE_PLATFORM=forgejo \
   -e RENOVATE_ENDPOINT=https://forgejo.alexmickelson.guru \
