51340099a467db05de4e687c8447f1633ef76766
Alex Mickelson <alexmickelson96@gmail.com>
2026-09-23 16:58:32 -0600

    update

--- nix/tv-computer/flake.nix
+++ nix/tv-computer/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "tv-computer NixOS configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs =
     { nixpkgs, ... }:
